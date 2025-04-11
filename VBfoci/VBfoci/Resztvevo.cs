9ca34eff1da9ef5add45563e54d278baef921942
palpet478 <115153266+Petya6666@users.noreply.github.com>
2025-04-11 13:04:53 +0200

    3.

--- VBfoci/VBfoci/Resztvevo.cs
+++ VBfoci/VBfoci/Resztvevo.cs
@@ -8,6 +8,20 @@ namespace VBfoci
 {
     internal class Resztvevo
     {
+        public string Orszag;
+        public int Helyezes;
+        public int VB_Idopont;
+        public string VB_Hely;
 
+
+        public Resztvevo(string sor) { 
+               string[] adatok = sor.Split(';');
+               Orszag = adatok[0];
+               Helyezes = int.Parse(adatok[1]);
+               VB_Idopont = int.Parse(adatok[2]);
+               VB_Hely = adatok[3];
+        
+        
+        }
     }
 }
