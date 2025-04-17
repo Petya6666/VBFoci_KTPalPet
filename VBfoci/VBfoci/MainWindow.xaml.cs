702fe3c3fcbe42d99ebcb2d7cd1671dbc8b6e1d2
Tomi126 <115153488+Tomi126@users.noreply.github.com>
2025-04-17 17:44:14 +0200

    kész program kommentek nélkül

--- VBfoci/VBfoci/MainWindow.xaml.cs
+++ VBfoci/VBfoci/MainWindow.xaml.cs
@@ -26,10 +26,11 @@ namespace VBfoci
         }
 
         List<Resztvevo> resztvevok = new List<Resztvevo>();
+        List<string> mentendoSorok = new List<string>();
 
         private void beolvasBtn_Click(object sender, RoutedEventArgs e)
         {
-            string fajlnev = "VBfoci.csv";
+            string fajlnev = fajlbeolvasTxt.Text;
             StreamReader sr = new StreamReader(fajlnev, Encoding.Default);
             while (!sr.EndOfStream)
             {
@@ -57,8 +58,92 @@ namespace VBfoci
         private void kiirasBtn_Click(object sender, RoutedEventArgs e)
         {
             string fajlnev = kiirasTxtbox.Text;
-            StreamWriter sw = new StreamWriter(fajlnev);
-            sw.Close();
+            using (StreamWriter sw = new StreamWriter(fajlnev))
+            {
+                foreach (string sor in mentendoSorok)
+                {
+                    sw.WriteLine(sor);
+                }
+            }
+        }
+
+        private void OKeresesBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string orszag = OTextbox.Text;
+            beolvasTxtBx.Text = $"{orszag} szereplései:\n";
+
+            mentendoSorok.Add($"{orszag} szereplései:");
+
+            for (int i = 0; i < resztvevok.Count; i++)
+            {
+                if (resztvevok[i].Orszag == orszag)
+                {
+                    string sor = $"Év: {resztvevok[i].VB_Idopont}, Helyezés: {resztvevok[i].Helyezes}";
+                    if (top.IsChecked == true)
+                    {
+                        if (resztvevok[i].Helyezes <= 5)
+                        {
+                            beolvasTxtBx.Text += $"Év: {resztvevok[i].VB_Idopont}, Helyezés: {resztvevok[i].Helyezes}\n";
+                            mentendoSorok.Add(sor);
+                        }
+                    }
+                    else
+                    {
+                        beolvasTxtBx.Text += $"Év: {resztvevok[i].VB_Idopont}, Helyezés: {resztvevok[i].Helyezes}\n";
+                        mentendoSorok.Add(sor);
+                    }
+                }
+            }
+            mentendoSorok.Add(""); // sorkihagyás a végére
+        }
+
+        private void HelyezesBtn_Click(object sender, RoutedEventArgs e)
+        {
+            int ev = Convert.ToInt32(evTxt.Text);
+            string eredmeny = $"Szűrés év szerint: {ev}\n";
+
+            foreach (var r in resztvevok)
+            {
+                if (r.VB_Idopont == ev)
+                {
+                    if (top3.IsChecked == true && r.Helyezes >= 1 && r.Helyezes <= 3)
+                    {
+                        eredmeny += $"{r.Orszag} - {r.Helyezes}\n";
+                    }
+                    else if (top10.IsChecked == true && r.Helyezes >= 4 && r.Helyezes <= 10)
+                    {
+                        eredmeny += $"{r.Orszag} - {r.Helyezes}\n";
+                    }
+                    else if (mindenki.IsChecked == true)
+                    {
+                        eredmeny += $"{r.Orszag} - {r.Helyezes}\n";
+                    }
+                }
+            }
+
+            beolvasTxtBx.Text = eredmeny;
+            mentendoSorok.Add(eredmeny); 
+        }
+
+        private void HelyzetKeresesBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (helyezesCombo.SelectedItem is ComboBoxItem selectedItem)
+            {
+                int keresettHelyezes = int.Parse(selectedItem.Content.ToString());
+                beolvasTxtBx.Text = $"{keresettHelyezes}. helyezettek listája:\n";
+                mentendoSorok.Add($"{keresettHelyezes}. helyezettek listája:"); 
+
+                var evSzerint = resztvevok
+                    .Where(r => r.Helyezes == keresettHelyezes)
+                    .OrderBy(r => r.VB_Idopont);
+
+                foreach (var r in evSzerint)
+                {
+                    string sor = $"{r.VB_Idopont}: {r.Orszag}";
+                    beolvasTxtBx.Text += sor + "\n";
+                    mentendoSorok.Add(sor); 
+                }
+            }
         }
     }
 }
